f78ae64a6d053de478445abe37bdc540c5b1253c
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-16 23:06:56 +0530

    making success urls

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .forms import ContactForm
 from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 class ContactFormView(FormView):
     template_name='app/home.html'
     form_class=ContactForm
+    success_url='/thanku/'
+
+class Thank(TemplateView):
+    template_name='app/thanku'
