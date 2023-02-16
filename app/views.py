40fe6f49827f424afd5508e07b477b6691ce4d1d
Aamir Ashraf <aamirnasa97@gmail.com>
2023-02-16 22:41:43 +0530

    making views and urls

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.views.generic.edit import FormView
 
 # Create your views here.
+class ContactFormView(FormView):
+    template_name='app/home.html'
+    form_class=ContactForm
